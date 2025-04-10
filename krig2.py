19f71cce997507338ce58587bc5789975d8e6ed9
Irish Apale <irishapale35@gmail.com>
2025-04-10 23:21:23 +0100

    Update krig2.py

--- krig2.py
+++ krig2.py
@@ -1,4 +1,4 @@
-#make sure that this file is in the same directory or folder as the krigmap_pro.py
+#make sure that this file is in the same directory or folder as the krig1.py
 import sys  # System-specific parameters and functions
 from itertools import zip_longest
 import os
